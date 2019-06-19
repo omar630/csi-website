ee2626a6e0801f838b7a9242b1b69a32dd00d878
mohammed omar <omarmd2311@gmail.com>
2019-06-19 15:25:34 +0530

    indentation done
    Co-Authored-By: omar630 <omarmd2311@gmail.com>

--- cmrec-csi/resources/views/navbar.blade.php
+++ cmrec-csi/resources/views/navbar.blade.php
@@ -1,45 +1,51 @@
 <nav class="navbar navbar-expand-lg navbar-dark sticky-top" style="background:#002060 !important;margin-top:0px;">
-        <img src="http://www.csi-india.org/images/logo.jpg" class="image-responsive d-block 
+    <img src="http://www.csi-india.org/images/logo.jpg" class="image-responsive d-block 
                           d-md-none" style="min-height:40px !important;min-width:40px !important;max-height:40px 
                                                                !important;max-width:40px !important">
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#csimenu" 
-                            aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-             <span class="navbar-toggler-icon" style="color:#fff !important"></span>
-          </button>
-        
-        
-      
-          <div class="collapse navbar-collapse" id="csimenu">
-               <ul class="navbar-nav mr-auto" id="ulmenu">
-                      <li class="nav-item dropdown1" style="margin-left:auto;margin-right:auto; max-width:150px;">
-                         <a class="dropbtn btn-block" href="###" ><i class="fa fa-sticky-note-o" aria-hidden="true"></i> Home</a><ul></ul>
-                      </li>
-                      <li class="nav-item dropdown1" style="margin-left:auto;margin-right:auto; max-width:150px;">
-                        <a class="dropbtn btn-block" href="{{url('news')}}" ><i class="fa fa-home" aria-hidden="true"></i> News<img src="http://www.csi-india.org/images/new.gif"></a><ul></ul>
-                      </li> 
-                      <li class="nav-item dropdown1" style="margin-left:auto;margin-right:auto;max-width:150px;">
-                        <a class="dropbtn btn-block" href="####" ><i class="fa fa-globe" aria-hidden="true"></i> About us </a><ul></ul>
-                      </li>
-                      <li class="nav-item dropdown1" style="margin-left:auto;margin-right:auto; max-width:180px;">
-                          <a class="dropbtn btn-block" href="###" ><i class="fa fa-user" aria-hidden="true"></i> Member's Exclusive</a><ul></ul>
-                      </li>
-                      <li class="nav-item dropdown1" style="margin-left:auto;margin-right:auto; min-width:200px;max-width:220px;">
-                          <a class="dropbtn btn-block" href="####" ><i class="fa fa-envelope" aria-hidden="true"></i> Contact us</a><ul></ul>
-                      </li>
-                      <li class="nav-item dropdown1" style="margin-left:auto;margin-right:auto; max-width:150px;">
-                           <a class="dropbtn btn-block" href="####" ><i class="fa fa-sticky-note-o" aria-hidden="true"></i> Posts</a><ul></ul>
-                      </li>
-                      <li class="nav-item dropdown1" style="margin-left:auto;margin-right:auto; max-width:150px;">
-                           <a class="dropbtn btn-block" href="####" > <i class="fa fa-sign-in" aria-hidden="true"></i> Log-in</a><ul></ul>
-                      </li>
-                    
-                 </ul>
-
-            <!-- if required a login at top-->
-            <!--form class="form-inline my-2 my-lg-0">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#csimenu"
+        aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon" style="color:#fff !important"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="csimenu">
+        <ul class="navbar-nav mr-auto" id="ulmenu">
+            <li class="nav-item dropdown1" style="margin-left:auto;margin-right:auto; max-width:150px;">
+                <a class="dropbtn btn-block" href="###"><i class="fa fa-sticky-note-o" aria-hidden="true"></i> Home</a>
+                <ul></ul>
+            </li>
+            <li class="nav-item dropdown1" style="margin-left:auto;margin-right:auto; max-width:150px;">
+                <a class="dropbtn btn-block" href="{{url('news')}}"><i class="fa fa-home" aria-hidden="true"></i>
+                    News<img src="http://www.csi-india.org/images/new.gif"></a>
+                <ul></ul>
+            </li>
+            <li class="nav-item dropdown1" style="margin-left:auto;margin-right:auto;max-width:150px;">
+                <a class="dropbtn btn-block" href="####"><i class="fa fa-globe" aria-hidden="true"></i> About us </a>
+                <ul></ul>
+            </li>
+            <li class="nav-item dropdown1" style="margin-left:auto;margin-right:auto; max-width:180px;">
+                <a class="dropbtn btn-block" href="###"><i class="fa fa-user" aria-hidden="true"></i> Member's
+                    Exclusive</a>
+                <ul></ul>
+            </li>
+            <li class="nav-item dropdown1" style="margin-left:auto;margin-right:auto; min-width:200px;max-width:220px;">
+                <a class="dropbtn btn-block" href="####"><i class="fa fa-envelope" aria-hidden="true"></i> Contact
+                    us</a>
+                <ul></ul>
+            </li>
+            <li class="nav-item dropdown1" style="margin-left:auto;margin-right:auto; max-width:150px;">
+                <a class="dropbtn btn-block" href="####"><i class="fa fa-sticky-note-o" aria-hidden="true"></i>
+                    Posts</a>
+                <ul></ul>
+            </li>
+            <li class="nav-item dropdown1" style="margin-left:auto;margin-right:auto; max-width:150px;">
+                <a class="dropbtn btn-block" href="####"> <i class="fa fa-sign-in" aria-hidden="true"></i> Log-in</a>
+                <ul></ul>
+            </li>
+        </ul>
+        <!-- if required a login at top-->
+        <!--form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="text" placeholder="Search">
             <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
             </form-->
-          </div>
+    </div>
 </nav>
-      <br>
+<br>
